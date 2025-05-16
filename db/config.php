64e6eb178d2ab075fc0a53f647f4441ce4851ac7
Sultan Alzahrani <140906935+SultanAlzahrani@users.noreply.github.com>
2025-05-16 04:03:47 +0300

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql112.infinityfree.com";  // replace with the actual host from your control panel
-$user = "if0_38729194";     // from MySQL sectionm
-$password = "vMCH4jlg1S51k"; // your MySQL password
-$dbname = "if0_38729194_my_Database";    // usually something like u123456789_yoursite
+$host = 
+$user = 
+$password =
+$dbname = 
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
